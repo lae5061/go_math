b8b4aacb720dedea99b8f2b2b496568647dcb284
Luc Eleazar <le250055@ncr.com>
2020-04-30 17:07:03 -0400

    zero not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,5 +1,11 @@
 package geometry
 
-func Volume(n int) int {
-	return n * n * n
+import "errors"
+
+// Volume calculate volume of cube
+func Volume(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("Zero length edge is not allowed")
 }
